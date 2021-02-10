1e818cf561f0b12c61e6c75540bff33caa92aa81
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-02-10 11:48:33 +0530

    02 Create hardcoded Limits-service
    Get limit values from configuration file.

---
+++ limits-service/src/main/java/com/lekhraj/spring/ms/limitsservice/configuration/Configuration.java
@@ -0,0 +1,29 @@
+package com.lekhraj.spring.ms.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+// provide service name above from pom artifact id.
+public class Configuration {
+	private int minimum;
+	private int maximum;
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
