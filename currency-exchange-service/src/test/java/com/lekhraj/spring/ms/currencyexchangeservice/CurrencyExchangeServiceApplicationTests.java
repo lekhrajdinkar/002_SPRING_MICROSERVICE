c9ff8d8396b811b301e3caaa188320e0b9eac2b5
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-02-10 19:04:55 +0530

    exchange service

---
+++ currency-exchange-service/src/test/java/com/lekhraj/spring/ms/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekhraj.spring.ms.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
