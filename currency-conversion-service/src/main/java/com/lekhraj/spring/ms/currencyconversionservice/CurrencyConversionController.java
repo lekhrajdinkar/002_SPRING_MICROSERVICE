0ad60fde99454ec2b06af0b87493715a3bde83c3
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-02-10 22:34:01 +0530

    invoke ms1 from ms2 using proxy - feign

--- currency-conversion-service/src/main/java/com/lekhraj/spring/ms/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/lekhraj/spring/ms/currencyconversionservice/CurrencyConversionController.java
@@ -3,27 +3,70 @@ package com.lekhraj.spring.ms.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class CurrencyConversionController {
+public class CurrencyConversionController 
+{
+	@Autowired
+	private CurrencyExchangeProxy proxy;
 	
-	
-	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	@GetMapping("/currency-conversion/v1/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from,
 			@PathVariable String to,
 			@PathVariable BigDecimal quantity
-			) {
-				
-		return new CurrencyConversion(10001L, 
+			) 
+	{
+		// way-1 : Tedious
+		//invoke exchange service from this service, notice  url of other ms below.
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from",from);
+		uriVariables.put("to",to);
+		
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
+		("http://localhost:8001/currency-exchange/from/{from}/to/{to}", 
+				CurrencyConversion.class, uriVariables);
+		
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
 				from, to, quantity, 
-				BigDecimal.ONE, 
-				BigDecimal.ONE, 
-				"");
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment()+ " " + "rest template");
 		
 	}
+	
+	@GetMapping("/currency-conversion/v2/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversion2(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) 
+	{
+		// way-2 : feign
+		//invoke exchange service from this service using better cloud solution - feign
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from",from);
+		uriVariables.put("to",to);
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment()+ " " + "reign");
+		
+	}
+
 
 }
