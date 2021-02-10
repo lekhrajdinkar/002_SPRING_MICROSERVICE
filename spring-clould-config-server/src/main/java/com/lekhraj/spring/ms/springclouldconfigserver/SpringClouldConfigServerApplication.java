971886a3b7ed1479bfb8153d831e1dc85181426a
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-02-10 13:21:06 +0530

    config server
    @EnableConfigServer

---
+++ spring-clould-config-server/src/main/java/com/lekhraj/spring/ms/springclouldconfigserver/SpringClouldConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.lekhraj.spring.ms.springclouldconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClouldConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClouldConfigServerApplication.class, args);
+	}
+
+}
---
+++ spring-clould-config-server/src/test/java/com/lekhraj/spring/ms/springclouldconfigserver/SpringClouldConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekhraj.spring.ms.springclouldconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClouldConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
