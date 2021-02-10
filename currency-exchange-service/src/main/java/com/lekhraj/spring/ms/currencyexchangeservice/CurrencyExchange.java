9eb4e22dfd2db7e0ce6e2655dc47f23d1f87d03d
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-02-10 20:21:42 +0530

    added jpa and h2

--- currency-exchange-service/src/main/java/com/lekhraj/spring/ms/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/lekhraj/spring/ms/currencyexchangeservice/CurrencyExchange.java
@@ -2,12 +2,24 @@ package com.lekhraj.spring.ms.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
 	
+	@Id
 	private Long id;
+	
+	@Column(name = "currency_from")
 	private String from;
+	
+	@Column(name = "currency_to")
 	private String to;
+	
 	private BigDecimal conversionMultiple;
+	private String environment;
 
 	public CurrencyExchange() {
 		
@@ -51,6 +63,14 @@ public class CurrencyExchange {
 
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
 	}
 
 	
--- currency-exchange-service/src/main/java/com/lekhraj/spring/ms/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/lekhraj/spring/ms/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,19 +2,38 @@ package com.lekhraj.spring.ms.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CurrencyExchangeRepository repository;
 		
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
-		return new CurrencyExchange(1000L, from, to, 
-						BigDecimal.valueOf(50));
+		
+		String port = environment.getProperty("local.server.port");
+		
+		
+		 //CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to,BigDecimal.valueOf(50));
+		 CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		 if(currencyExchange ==null) {
+				throw new RuntimeException
+					("Unable to Find data for " + from + " to " + to);
+			}
+		 currencyExchange.setEnvironment(port);
+		 
+		 return currencyExchange ;
 		
 	}
 
